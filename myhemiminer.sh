00cf7b853b5d33f744b61f4e305c3186943a01e9
HAHA0822 <yijianming1024@gmail.com>
2024-09-21 21:15:35 +0800

    Add files via upload

--- myhemiminer.sh
+++ myhemiminer.sh
@@ -83,15 +83,15 @@ install_pm2() {
 # 后台实时更新 POPM_STATIC_FEE 的函数
 update_fee_in_background() {
     local log_file="$HOME/heminetwork/update_fee.log"
-    local threshold=350  # 定义阈值，当 optimal_fee 超过该值时不再更新
+    local threshold=300  # 定义阈值，当 optimal_fee 超过该值时不再更新
 
     while true; do
         export POPM_STATIC_FEE=${POPM_STATIC_FEE:-1}  # 确保环境变量可用
         printf "当前费率为 $POPM_STATIC_FEE\n" >> "$log_file"
 
-        current_fee=$(curl -s https://mempool.space/testnet/api/v1/fees/recommended | jq .fastestFee)
+        current_fee=$(curl -s https://mempool.space/testnet/api/v1/fees/recommended | jq .economyFee)
         # current_fee = $average_fee
-        optimal_fee=$(($current_fee + 5))
+        optimal_fee=$(($current_fee + 10))
 
         # 检查 optimal_fee 是否超过阈值
         if [ "$optimal_fee" -le "$threshold" ]; then
@@ -133,13 +133,13 @@ download_and_setup() {
 setup_environment() {
     cd "$HOME/heminetwork"
     cat ~/popm-address.json
-    local threshold=350  # 定义阈值
+    local threshold=300  # 定义阈值
 
-    current_fee=$(curl -s https://mempool.space/testnet/api/v1/fees/recommended | jq .fastestFee)
+    current_fee=$(curl -s https://mempool.space/testnet/api/v1/fees/recommended | jq .economyFee)
 
     if [ "$current_fee" -le "$threshold" ]; then
             # 更新环境变量
-            optimal_fee=$(($current_fee + 5))
+            optimal_fee=$(($current_fee + 10))
     else
             optimal_fee=$(($threshold))
     fi
